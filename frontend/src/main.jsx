b2a782c4a3afbce89166579034fd84192d999dfa
redU5 <fufix71@gmail.com>
2026-02-09 14:36:32 +0200

    front + back

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,10 @@
-// src/main.jsx
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router } from 'react-router-dom';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './styles/global.css'
 import App from './App.jsx'
-import './index.css'
-import { UserProvider } from './contexts/UserContext.jsx'; // הוספנו
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Router>
-      <UserProvider> {/* עוטפים את כל האפליקציה ב-UserContext */}
-        <App />
-      </UserProvider>
-    </Router>
-  </React.StrictMode>,
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
